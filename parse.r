09704114dffbec5ef3d7aa180a4aab1baa3292d5
hiroki yagi <hiro.yagi0245@gmail.com>
2016-10-27 13:10:01 +0900

    add pairlist

--- parse.r
+++ parse.r
@@ -1,44 +1,55 @@
+parseFile <- function(file) {
+  exp <- parse(file)
+  lapply(exp, parseStatement)
+}
+
 parseString <- function(str) {
-    exp <- parse(text = str)
-    lapply(exp, parseStatement)
+  exp <- parse(text = str)
+  lapply(exp, parseStatement)
 }
 
 parseStatement <- function(lang, inner = FALSE) {
-    ## call
-    if (is.call(lang)) {
-        op <- operator(lang)
-        if (inner) cat(" ")             # print
-        cat("(")                        # print
-        printOperator(op)               # print
+  ## call
+  if (is.call(lang)) {
+    op <- operator(lang)
+    if (inner) cat(" ")             # print
+    cat("(")                        # print
+    printOperator(op)               # print
 
-        result <- append(op,
-                         lapply(operands(lang),
-                                function(opds){parseStatement(opds, TRUE)}))
-        cat(")")                        # print
-        if (!inner) cat(" \n")          # print
-
-        result
-    } else {
-        ## atomic
-        printOperand(lang)              # print
-        lang
-    }
+    result <- append(op,
+                     lapply(operands(lang),
+                            function(opds){parseStatement(opds, TRUE)}))
+    cat(")")                        # print
+    if (!inner) cat(" \n")          # print
+    result
+  } else if (is.pairlist(lang)){
+    cat("pairlist")
+    lang
+  } else {
+    ## atomic
+    printOperand(lang)              # print
+    lang
+  }
 }
 
 operator <- function(call) {
-    call[[1]]
+  call[[1]]
 }
 
 operands <- function(call) {
-    call[2:length(call)]
+  call[2:length(call)]
 }
 
 printOperator <- function(op) {
-    if (isGrammarSymbol(op)) cat("syntax:")
-    cat( paste("\"", op, "\"", sep = "") )
+  if (isGrammarSymbol(op)) cat("syntax:")
+  cat( paste("\"", op, "\"", sep = "") )
 }
 
 printOperand <- function(opd, inner = FALSE) {
-    cat(" ")
-    cat(opd)
+  cat(" ")
+  cat(opd)
+}
+
+printPairlist <- function(pl) {
+  
 }
--- pryr/R/draw-tree.r
+++ pryr/R/draw-tree.r
@@ -18,14 +18,14 @@
 #' ast(f()()())
 #' @importFrom stringr str_c
 call_tree <- function(x, width = getOption("width")) {
-    if (is.expression(x) || is.list(x)) {
-        trees <- vapply(x, tree, character(1), width = width)
-        out <- str_c(trees, collapse = "\n\n")
-    } else {
-        out <- tree(x, width = width)
-    }
+  if (is.expression(x) || is.list(x)) {
+    trees <- vapply(x, tree, character(1), width = width)
+    out <- str_c(trees, collapse = "\n\n")
+  } else {
+    out <- tree(x, width = width)
+  }
 
-    cat(out, "\n")
+  cat(out, "\n")
 }
 
 #' @rdname call_tree
@@ -34,54 +34,54 @@ ast <- function(x) call_tree(substitute(x))
 
 #' @importFrom stringr str_c str_length str_sub
 str_trunc <- function(x, width = getOption("width")) {
-    ifelse(str_length(x) <= width, x, str_c(str_sub(x, 1, width - 3), "..."))
+  ifelse(str_length(x) <= width, x, str_c(str_sub(x, 1, width - 3), "..."))
 }
 
 #' @importFrom stringr str_c str_dup
 tree <- function(x, level = 1, width = getOption("width"), branch = "\\- ") {
-    indent <- str_c(str_dup("  ", level - 1), branch)
-    
-    if (is.atomic(x) && length(x) == 1) {
-        label <- paste0(" ", deparse(x)[1])
-        children <- NULL
-    } else if (is.name(x)) {
-        x <- as.character(x)
-        if (x == "") {
-            ## Special case the missing argument
-            label <- "`MISSING"
-        } else {
-            label <- paste0("`", as.character(x))
-        }
-
-        children <- NULL
-    } else if (is.call(x)) {
-        label <- "()"
-        children <-  vapply(as.list(x), tree, character(1),
-                            level = level + 1, width = width - 3)
-    } else if (is.pairlist(x)) {
-        label <- "[]"
-
-        branches <- paste("\\", format(names(x)), "=")
-        children <- character(length(x))
-        for (i in seq_along(x)) {
-            children[i] <- tree(x[[i]], level = level + 1, width = width - 3,
-                                branch = branches[i])
-        }
+  indent <- str_c(str_dup("  ", level - 1), branch)
+  
+  if (is.atomic(x) && length(x) == 1) {
+    label <- paste0(" ", deparse(x)[1])
+    children <- NULL
+  } else if (is.name(x)) {
+    x <- as.character(x)
+    if (x == "") {
+      ## Special case the missing argument
+      label <- "`MISSING"
     } else {
-        ## Special case for srcrefs, since they're commonly seen
-        if (inherits(x, "srcref")) {
-            label <- "<srcref>"
-        } else {
-            label <- paste0("<", typeof(x), ">")
-        }
-        children <- NULL
+      label <- paste0("`", as.character(x))
     }
 
-    label <- str_trunc(label, width - 3)
+    children <- NULL
+  } else if (is.call(x)) {
+    label <- "()"
+    children <-  vapply(as.list(x), tree, character(1),
+                        level = level + 1, width = width - 3)
+  } else if (is.pairlist(x)) {
+    label <- "[]"
 
-    if (is.null(children)) {
-        paste0(indent, label)
-    } else {
-        paste0(indent, label, "\n", paste0(children, collapse = "\n"))
+    branches <- paste("\\", format(names(x)), "=")
+    children <- character(length(x))
+    for (i in seq_along(x)) {
+      children[i] <- tree(x[[i]], level = level + 1, width = width - 3,
+                          branch = branches[i])
     }
+  } else {
+    ## Special case for srcrefs, since they're commonly seen
+    if (inherits(x, "srcref")) {
+      label <- "<srcref>"
+    } else {
+      label <- paste0("<", typeof(x), ">")
+    }
+    children <- NULL
+  }
+
+  label <- str_trunc(label, width - 3)
+
+  if (is.null(children)) {
+    paste0(indent, label)
+  } else {
+    paste0(indent, label, "\n", paste0(children, collapse = "\n"))
+  }
 }
