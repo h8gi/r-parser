b55fc2ddc9df5e73b2862b0df2b2b83398fc2341
yagi <hiro.yagi0245@gmail.com>
2016-10-27 12:32:29 +0900

    first commit

---
+++ parse.r
@@ -0,0 +1,44 @@
+parseString <- function(str) {
+    exp <- parse(text = str)
+    lapply(exp, parseStatement)
+}
+
+parseStatement <- function(lang, inner = FALSE) {
+    ## call
+    if (is.call(lang)) {
+        op <- operator(lang)
+        if (inner) cat(" ")             # print
+        cat("(")                        # print
+        printOperator(op)               # print
+
+        result <- append(op,
+                         lapply(operands(lang),
+                                function(opds){parseStatement(opds, TRUE)}))
+        cat(")")                        # print
+        if (!inner) cat(" \n")          # print
+
+        result
+    } else {
+        ## atomic
+        printOperand(lang)              # print
+        lang
+    }
+}
+
+operator <- function(call) {
+    call[[1]]
+}
+
+operands <- function(call) {
+    call[2:length(call)]
+}
+
+printOperator <- function(op) {
+    if (isGrammarSymbol(op)) cat("syntax:")
+    cat( paste("\"", op, "\"", sep = "") )
+}
+
+printOperand <- function(opd, inner = FALSE) {
+    cat(" ")
+    cat(opd)
+}
